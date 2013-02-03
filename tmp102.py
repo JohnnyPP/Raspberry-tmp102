06b70e29d4e2c94709cd22ff0fc4d521b40a3637
JohnnyPP <johnnnyppp@gmail.com>
2013-02-03 17:18:53 +0000

    real and corrected temperatures

--- tmp102.py
+++ tmp102.py
@@ -1,5 +1,6 @@
 import smbus
 import time
+
 bus=smbus.SMBus(1)
 address=0x48
 
@@ -10,10 +11,27 @@ def tmp102Read():
 def TemperatureConversion():
         val = (LSB << 4)
         val |= (MSB >> 4)
-        convertedtemp = val*0.0625
+        convertedtemp = val*0.0625+1
         return convertedtemp
 
-for x in range(30):
+def TemperatureConversionNewVersion():
+        '''
+        #the received tmp102 data must be reordered
+        e.g. received data=0x2011 (LSB=0x11, MSB=0x20)
+        must be translated to 0x1120. 
+        LSB<<8 gives additional 2 nulls 0x1100 now one may combine it with MSB
+        reverseddata=LSBtoWord|MSB the returned value is 0x1120
+        next reversed data must be shifted by 4 (according to tmp102 documentation)
+        finally the result must be multiplied by 0.0625 convertedtemp = reverseddata*0.0625
+        '''
+        LSBtoWord = (LSB<<8)
+        reverseddata = LSBtoWord|MSB
+        reverseddata = reverseddata>>4
+        convertedtemp = reverseddata*0.0625
+        return convertedtemp
+
+
+for x in range(5):
         iTmp102 = tmp102Read()
         LSB = iTmp102 % 256
         MSB = iTmp102 / 256
@@ -22,6 +40,7 @@ for x in range(30):
         print('LSB byte ' + str(hex(LSB)))
         print('MSB byte ' + str(hex(MSB)))
         print("%.4f" % TemperatureConversion())
+        print("%.4f" % TemperatureConversionNewVersion())
         print
         time.sleep(1)
         
